bf5144e5ee37d909d5160d34c799aac79e8683b6
Kaua-Silva-Eshows <kaua.silva@eshows.com.br>
2025-05-15 16:25:03 -0300

    Add files via upload

---
+++ menu/likely_artists.py
@@ -0,0 +1,53 @@
+from datetime import date, datetime, timedelta
+import streamlit as st
+from data.querys_eshows import *
+from data.querys_apis.asaas_api import *
+from menu.page import Page
+from utils.components import *
+from utils.functions import *
+
+def BuildLikelyArtists(rankliKelyArtists):
+
+    row = st.columns(6)
+    global day, day2
+
+    with row[2]:
+        day = st.date_input('Data Inicio:',value=date.today() - timedelta(days=1),format='DD/MM/YYYY',key='day')
+    
+    with row[3]:
+        day2 = st.date_input('Data Fim:',value=date.today() - timedelta(days=1),format='DD/MM/YYYY',key='day2')
+
+    
+    rankliKelyArtists = rank_likely_artists(day, day2, filters='', id_group='')
+    
+    row1 = st.columns(3)
+    with row1[1]:
+        opportunity = len(rankliKelyArtists['ID_OPORTUNIDADE'].unique())
+        tile = row1[1].container(border=True)
+        tile.write(f"""<p style='text-align: center; font-size: 15px;'>Oportunidades em Aberto<br><span style='font-size: 17px;'>{opportunity}</span></p>""",unsafe_allow_html=True)
+
+        companie = st.selectbox ("Estabelecimento", rankliKelyArtists['Estabelecimento'].unique())
+    
+    rankliKelyArtists = rankliKelyArtists[rankliKelyArtists['Estabelecimento'] == companie]
+    id_group = rankliKelyArtists['ID GRUPO'].unique()
+    id_companie = rankliKelyArtists['ID Estabelecimento'].unique()
+
+    filters = f"AND O.FK_CONTRATANTE = '{id_companie[0]}'"
+
+    rankliKelyArtists = rank_likely_artists(day, day2, filters, id_group[0])
+    
+    rankliKelyArtists = rankliKelyArtists.drop(columns=['ID Estabelecimento', 'Estabelecimento', 'ID GRUPO', 'ID Artista', 'RN'])
+    function_format_number_columns
+    filtered_copy, count = component_plotDataframe(rankliKelyArtists, 'Artistas Mais Provaveis')
+    function_copy_dataframe_as_tsv(filtered_copy)
+
+    st.markdown("---")
+
+class LikelyArtists(Page):
+    def render(self):
+        self.data = {}
+        day = date.today() - timedelta(days=1)
+        day2 = date.today() - timedelta(days=1)
+        self.data['rankliKelyArtists'] = rank_likely_artists(day, day2, filters='', id_group='')
+        
+        BuildLikelyArtists(self.data['rankliKelyArtists'])
