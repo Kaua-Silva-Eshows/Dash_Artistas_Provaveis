f16a68123054f00128e3ab5071fbf1bc4344ad56
Kaua-Silva-Eshows <kaua.silva@eshows.com.br>
2025-07-29 13:45:51 -0300

    Add files via upload

--- pages/home.py
+++ pages/home.py
@@ -3,11 +3,16 @@ from menu.page import Page
 from utils.components import *
 from utils.user import logout
 from menu.likely_artists import LikelyArtists
+from streamlit_theme import st_theme
+
 def render():
     # pegando dados da sessão como ID e NOME
     user_id = st.session_state['user_data']["data"]["user_id"]
     user_name = st.session_state['user_data']["data"]['full_name']
-
+    theme = st_theme(key=f"theme_")
+    base_theme = theme.get("base") if theme else "default"
+    st.session_state["base_theme"] = base_theme
+    
     col1, col2, col3 = st.columns([3.5,0.5,0.3])
     
     col1.write(f"## Olá, "+user_name)
